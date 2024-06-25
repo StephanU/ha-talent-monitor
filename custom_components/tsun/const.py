1a52dc66aebea9075a2af5cd97230657c8d26a51
Stephan Uhle <stephanu@gmx.net>
2024-06-25 21:03:04 +0000

    Fixed domain

--- custom_components/tsun/const.py
+++ custom_components/tsun/const.py
@@ -3,7 +3,7 @@
 # Base component constants
 NAME = "TalentMonitor"
 MANUFACTURER = "TSUNESS Co., Ltd."
-DOMAIN = "talent_monitor"
+DOMAIN = "tsun"
 DOMAIN_DATA = f"{DOMAIN}_data"
 VERSION = "0.0.1"
 
